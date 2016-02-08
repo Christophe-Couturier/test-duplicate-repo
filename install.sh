ef4b692111c9ccef97b5119723322b627e901f87
Marios Makassikis <mmakassikis@gmail.com>
2016-02-08 13:56:34 +0100

    Update install.sh script.
    - Fix bug with unit files installation
    - Install itseml and ws scripts (they have to be in the same directory!)
    - Install helper scripts
    - Install templates
    - Install doc

--- install.sh
+++ install.sh
@@ -2,27 +2,41 @@
 
 DESTDIR="$1"
 
+UNIT_DIR=/lib/systemd/system
+DOC_DIR=/usr/share/doc/itseml
+TMPL_DIR=/var/lib/itseml
+
 # Unit templates
-install -o root -g root -m 644 -D eml-netns@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-itsnet@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-mwtun@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-mw-server@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-gpsfwd@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-gpsd@.service \
-				${DESTDIR}/lib/systemd/system
-install -o root -g root -m 644 -D eml-gpspipe@.service \
-				${DESTDIR}/lib/systemd/system
+find . -type f -iname '*.service' | while read f; do
+	install -o root -g root -m 644 -D ${f} \
+				${DESTDIR}/${UNIT_DIR}/$f
+done
 
 # Create directories
-mkdir -p ${DESTDIR}/etc/itseml/templates
+mkdir -p ${DESTDIR}/${DOC_DIR}
+mkdir -p ${DESTDIR}/${TMPL_DIR}
 
 # Helper scripts
 install -o root -g root -m 755 -D setup_netns \
 				${DESTDIR}/usr/bin/setup_netns
+install -o root -g root -m 755 -D gen_itsnet_conf \
+				${DESTDIR}/usr/bin/gen_itsnet_conf
+# Main script
 install -o root -g root -m 755 -D itseml.py \
 				${DESTDIR}/usr/bin/itseml
+sed -i ${DESTDIR}/usr/bin/itseml -e "s,^TMPL_PATH = .*,TMPL_PATH = \"${TMPL_DIR}/templates\","
+# WebSocket frontend
+install -o root -g root -m 755 -D ws.py \
+				${DESTDIR}/usr/bin/ws-itseml
+
+# Templates
+find templates -type f | while read f; do
+	install -o root -g root -m 644 -D ${f} \
+				${DESTDIR}/${TMPL_DIR}/$f
+done
+
+# Docs
+install -o root -g root -m 644 -D README \
+				${DESTDIR}/${DOC_DIR}/README
+install -o root -g root -m 644 -D sample.nginx.conf \
+				${DESTDIR}/${DOC_DIR}/sample.nginx.conf
