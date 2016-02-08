fdcf505e02fe1c49df8c9e256a07b806ad0973c7
Marios Makassikis <mmakassikis@gmail.com>
2016-02-08 14:55:01 +0100

    Make script more packaging friendly.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='itseml',
+      version='0.1',
+      description='ITS emulator helper',
+      author='Marios Makassikis',
+      author_email='marios.makassikis@yogoko.fr',
+      url='https://www.yogoko.fr',
+      packages=['itseml'],
+     )
