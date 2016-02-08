8d695196ba3038ca8434be30f76d6503f8abdee9
Marios Makassikis <mmakassikis@gmail.com>
2016-02-08 13:33:22 +0100

    Use spaces insteads of tabs.
    Let's be consistent with the rest of the code...

--- ws.py
+++ ws.py
@@ -5,11 +5,11 @@ import json
 
 class ItsApplication(WebSocketApplication):
     def on_message(self, message):
-	try:
-		response = itseml.process_message(json.loads(message))
-	except ValueError, e:
-		response = "400 Invalid JSON"
-	self.ws.send(response)
+        try:
+            response = itseml.process_message(json.loads(message))
+        except ValueError, e:
+            response = "400 Invalid JSON"
+        self.ws.send(response)
 
 WebSocketServer(
     ('', itseml.BASE_PORT),
