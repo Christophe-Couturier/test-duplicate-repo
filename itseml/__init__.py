Christophe-Couturier/test-duplicate-repo

commit 7c87ad1e9972f41706c6fd0683261d65e39cc578

Export IP configuration in a script started by a service
This is done as an alternative to calling the commands from the
controller script.

bin/eml-ip-setup
@@ -1,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import netaddr
+import subprocess
+import sys
+
+IP_NETWORK= "10.1.1.0/24"
+SUBNET_PLEN = 30
+
+def _network_conf(envnum):
+    network = netaddr.IPNetwork(IP_NETWORK)
+    subnets = list(network.subnet(SUBNET_PLEN))
+    net = subnets[envnum]
+
+    local = str(netaddr.IPAddress(net.first+1))
+    remt = str(netaddr.IPAddress(net.first+2))
+
+    cmds = []
+    cmds.append("ip netns exec env%d ip a a %s/%d dev eth1" % (envnum, remt, SUBNET_PLEN))
+    cmds.append("ip netns exec env%d ip r a default via %s" % (envnum, local))
+    cmds.append("ip a a %s/%d dev ctl%d" % (local, SUBNET_PLEN, envnum))
+    print("Applying IP configuration for env%d", envnum)
+    for cmd in cmds:
+        out = subprocess.check_call(cmd, shell=True)
+
+if __name__ == '__main__':
+    try:
+	envname = sys.argv[1].replace('env', '')
+        _network_conf(int(envname))
+    except IndexError:
+        print ("Missing parameter (envnum)")

itseml/itseml.py
@@ -12,7 +12,6 @@ import subprocess
 import sys
 import pprint
 import random
-import netaddr
 import json
 
 from shutil import copyfile
@@ -20,8 +19,6 @@ from string import Template
 
 TMPL_PATH = os.path.join(os.path.dirname(__file__), "templates")
 CONF_PATH = "/var/run/itseml/"
-IP_NETWORK= "10.1.1.0/24"
-SUBNET_PLEN = 30
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -40,7 +37,6 @@ def start_env(params, envname):
     generate_configuration(params, envname)
 
     _service_action('start', envname)
-    _network_conf(int(params['id']))
     if 'intersections' in params.get('map'):
         map_sender(params.get('map'), envname)
         mqtt_notify(envname)
@@ -56,24 +52,8 @@ def _get_if_mac(iface):
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', iface[:15]))
     return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 
-def _network_conf(envnum):
-    network = netaddr.IPNetwork(IP_NETWORK)
-    subnets = list(network.subnet(SUBNET_PLEN))
-    net = subnets[envnum]
-
-    local = str(netaddr.IPAddress(net.first+1))
-    remt = str(netaddr.IPAddress(net.first+2))
-
-    cmds = []
-    cmds.append("ip netns exec env%d ip a a %s/%d dev eth1" % (envnum, remt, SUBNET_PLEN))
-    cmds.append("ip netns exec env%d ip r a default via %s" % (envnum, local))
-    cmds.append("ip a a %s/%d dev ctl%d" % (local, SUBNET_PLEN, envnum))
-    logging.info("Applying IP configuration")
-    for cmd in cmds:
-        out = subprocess.check_call(cmd, shell=True)
-
 def _service_action(action, envname):
-    svcs = ['eml-netns', 'eml-itsnet', 'eml-mwtun', 'eml-mw-server']
+    svcs = ['eml-netns', 'eml-ip-setup', 'eml-itsnet', 'eml-mwtun', 'eml-mw-server']
 
     services = ' '.join([x + '@%s' % (envname) for x in svcs])
 

service_files/eml-ip-setup@.service
@@ -1,0 +1,13 @@
+[Unit]
+Description=Configure IP addressing for emulated ITS station
+After=network.target
+Requires=eml-netns@%i.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=true
+ExecStart=/usr/bin/eml-ip-setup %i
+
+[Install]
+WantedBy=multi-user.target
+

setup.py
@@ -12,11 +12,12 @@ setup(name='itseml',
       package_data={'itseml': ['templates/*', 'templates/mw/*',
                                'templates/mw/config/*']},
       scripts=['bin/gen_itsnet_conf', 'bin/setup_netns', 'bin/ws-itseml', 'bin/cli-itseml',
-               'bin/publish_mqtt', 'bin/send_mapjson'],
+               'bin/eml-ip-setup', 'bin/publish_mqtt', 'bin/send_mapjson'],
       data_files=[('/usr/share/doc/itseml', ['README', 'doc/sample.nginx.conf',
                                              'doc/sample_json/start.json',
                                              'doc/sample_json/stop.json']),
                   ('/lib/systemd/system', ['service_files/eml-netns@.service',
+                                           'service_files/eml-ip-setup@.service',
                                            'service_files/eml-itsnet@.service',
                                            'service_files/eml-mapsender@.service',
                                            'service_files/eml-mqtt-notify@.service',

commit fa7a76a0cb93e1a82fe971b7ae25428617c0a9f5

Commit 7c87ad1e99 broke status function. unbreak
We still need the env's IP in the main module to query the cache.

bin/eml-ip-setup
@@ -3,23 +3,16 @@
 import netaddr
 import subprocess
 import sys
-
-IP_NETWORK= "10.1.1.0/24"
-SUBNET_PLEN = 30
+from itseml import _addr_pair, SUBNET_PLEN
 
 def _network_conf(envnum):
-    network = netaddr.IPNetwork(IP_NETWORK)
-    subnets = list(network.subnet(SUBNET_PLEN))
-    net = subnets[envnum]
-
-    local = str(netaddr.IPAddress(net.first+1))
-    remt = str(netaddr.IPAddress(net.first+2))
+    local, remt = _addr_pair(envnum)
 
     cmds = []
     cmds.append("ip netns exec env%d ip a a %s/%d dev eth1" % (envnum, remt, SUBNET_PLEN))
     cmds.append("ip netns exec env%d ip r a default via %s" % (envnum, local))
     cmds.append("ip a a %s/%d dev ctl%d" % (local, SUBNET_PLEN, envnum))
-    print("Applying IP configuration for env%d", envnum)
+    print("Applying IP configuration for env%d" % (envnum))
     for cmd in cmds:
         out = subprocess.check_call(cmd, shell=True)
 

itseml/__init__.py
@@ -1,4 +1,4 @@
-from itseml import process_message, statistics
+from itseml import process_message, statistics, _addr_pair, SUBNET_PLEN
 
 __version__ = '0.1'
 __all__ = ['itseml']

itseml/itseml.py
@@ -4,6 +4,7 @@ import collections
 import distutils.dir_util
 import fcntl
 import logging
+import netaddr
 import os
 import os.path
 import socket
@@ -22,6 +23,9 @@ CONF_PATH = "/var/run/itseml/"
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+IP_NETWORK= "10.1.1.0/24"
+SUBNET_PLEN = 30
 
 # create formatter
 formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
@@ -46,6 +50,17 @@ def start_env(params, envname):
 def _mac_to_gn_addr(hw_addr):
     gn_addr = '0000' + hw_addr.replace(':', '')
     return ':'.join([gn_addr[i:i+4] for i in range(0, len(gn_addr), 4)])
+
+def _addr_pair(envnum):
+    """Return a tuple with local, remote IP addresses"""
+    network = netaddr.IPNetwork(IP_NETWORK)
+    subnets = list(network.subnet(SUBNET_PLEN))
+    net = subnets[envnum]
+
+    local = str(netaddr.IPAddress(net.first+1))
+    remote = str(netaddr.IPAddress(net.first+2))
+
+    return local, remote
 
 def _get_if_mac(iface):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)